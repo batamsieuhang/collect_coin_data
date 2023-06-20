b2b5ea365b5f77a2909a67480746f51db5242c2a
TuDo16 <20021460@vnu.edu.vn>
2023-06-20 16:53:28 +0700

    add gap in coin information

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time
 from urllib.request import Request, urlopen
 from take_value import take_data
-
+from datetime import datetime
 
 while (True):
     count_coin = {}
     data = take_data()
+    coin_gap_dict = {}
 
     for value in data.values():
         for coin in value:
@@ -13,10 +14,16 @@ while (True):
             if coin_name in count_coin:
                 count_coin[coin_name] += 1
             else:
+
+                coin_gap_dict[coin_name] = coin['gap']
                 count_coin[coin_name] = 1
     sort_count_coin = dict(sorted(
         count_coin.items(), key=lambda x: x[1], reverse=True))
-    print("---------------------------------------------------------------------------")
+    print("------------------------------{current_time}--------------------------------".format(
+        current_time=datetime.now()))
     print(sort_count_coin)
-    print("---------------------------------------------------------------------------\n")
+    for coin, value in sort_count_coin.items():
+        if value >= 4:
+            print(coin+":"+coin_gap_dict[coin])
+    print("-----------------------------end--------------------------------------------\n")
     time.sleep(10)
