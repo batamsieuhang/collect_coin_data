0a0fc4bc50db4be85085b1d58830ec69982c17dc
TuDo16 <20021460@vnu.edu.vn>
2023-06-20 16:25:28 +0700

    reformat form

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import requests
 import bs4
 import re
+import time
 from urllib.request import Request, urlopen
 from take_value import take_data
 
 
-count_coin = {}
-data = take_data()
+while (True):
+    count_coin = {}
+    data = take_data()
 
-for value in data.values():
-    for coin in value:
-        coin_name = coin['name']
-        if coin_name in count_coin:
-            count_coin[coin_name] += 1
-        else:
-            count_coin[coin_name] = 1
-print(count_coin)
-
-sort_count_coin = sorted(count_coin.items(), key=lambda x: x[1], reverse=True)
-
-print(dict(sort_count_coin))
+    for value in data.values():
+        for coin in value:
+            coin_name = coin['name']
+            if coin_name in count_coin:
+                count_coin[coin_name] += 1
+            else:
+                count_coin[coin_name] = 1
+    sort_count_coin = sorted(
+        count_coin.items(), key=lambda x: x[1], reverse=True)
+    print("---------------------------------------------------------------------------")
+    print(dict(sort_count_coin))
+    print("---------------------------------------------------------------------------\n")
+    time.sleep(10)
