63240d81a509cd5915b5363356b271e443c842f3
TuDo16 <20021460@vnu.edu.vn>
2023-06-23 15:47:16 +0700

    edit function

--- futures_long.py
+++ futures_long.py
@@ -1,45 +1,11 @@
-import time
-from take_value import take_data
-from datetime import datetime
-from count_coin import count_num_coin
+from results import print_results
 import sys
 
-time_value = sys.argv[1:(len(sys.argv)-1)]
-
-map_time_value = {"1m": 0, "2m": 1, "3m": 2,
-                  "4m": 3, "5m": 4, "10m": 5, "15m": 6, "20m": 7,"30m":8,"1h":9,"2h":10,"4h":11,"6h":12,"12h":13,"13h":14}
+# Variables
+time_value = sys.argv[1:(len(sys.argv)-2)]
 url = "https://ckcoin.top/Futures/Long"
-map_value = []
-
-for i in time_value:
-    for key, value in map_time_value.items():
-        if i == key:
-            map_value.append(value)
 sort_value = int(sys.argv[len(sys.argv)-2])
-
 time_delay =int(sys.argv[len(sys.argv)-1])
 
-process_count_coin = {}
-while (True):
-    count_coin = {}
-    coin_gap_dict = {}
 
-    count_coin, coin_gap_dict = count_num_coin(take_data(map_value,url))
-
-    sort_count_coin = dict(sorted(
-        count_coin.items(), key=lambda x: x[1], reverse=True))
-    print("----------{sort_value}--------------------{current_time}------------{time_value}--------".format(
-        current_time=datetime.now(),time_value=time_value,sort_value=sort_value))
-    print(sort_count_coin)
-    for coin, value in sort_count_coin.items():
-        if value >= sort_value:
-            if coin in process_count_coin:
-                process_count_coin[coin] += 1
-            else:
-                process_count_coin[coin] = 1
-
-    for coin, value in sort_count_coin.items():
-        if value >= sort_value:
-            print(coin+":"+"      "+coin_gap_dict[coin]+"      "+str(process_count_coin[coin]))
-    print("-----------------------------FUTURES_LONG--------------------------------------------\n")
-    time.sleep(time_delay)
+print_results(url,time_value,sort_value,time_delay)
